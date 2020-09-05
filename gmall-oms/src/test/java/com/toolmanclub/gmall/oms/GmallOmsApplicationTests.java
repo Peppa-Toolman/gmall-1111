1ef3b0e60e24e9d3c49e0fb98f3e8734e4a48c4d
username <email>
2020-09-05 16:28:56 +0800

    初始化-1

--- gmall-oms/src/test/java/com/toolmanclub/gmall/oms/GmallOmsApplicationTests.java
+++ gmall-oms/src/test/java/com/toolmanclub/gmall/oms/GmallOmsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.toolmanclub.gmall.oms;
 
+import com.toolmanclub.gmall.oms.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
+@Slf4j
 @SpringBootTest
 class GmallOmsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Resource
+    private OrderService orderService;
+
+    @Test
+    void contextLoads() {
+        int count = orderService.count();
+        log.info(String.valueOf(count));
+    }
 
 }
